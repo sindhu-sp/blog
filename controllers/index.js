31774d9fda8b27c5c4b84a280cc6838d5046eaf1
sindhu-sp <sindhupillaisuresh@gmail.com>
2021-08-18 02:30:08 -0700

    api routes added

---
+++ controllers/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+const userRoutes = require('./api/user-routes');
+const postRoutes = require('./api/post-routes');
+const commentRoutes = require('./api/comment-routes');
+
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
+
+module.exports = router;
