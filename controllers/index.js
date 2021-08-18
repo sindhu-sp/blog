192e7d287d27884eb06d5f4643afa40932e9ecd3
sindhu-sp <sindhupillaisuresh@gmail.com>
2021-08-18 02:45:34 -0700

    home-routes and dashboard-routes added

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,16 @@
 const router = require('express').Router();
 
-const userRoutes = require('./api/user-routes');
-const postRoutes = require('./api/post-routes');
-const commentRoutes = require('./api/comment-routes');
+const apiRoutes = require('./api/');
+const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
 
-router.use('/users', userRoutes);
-router.use('/posts', postRoutes);
-router.use('/comments', commentRoutes);
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
+
+
+router.use((req, res) => {
+    res.status(404).end();
+});
 
 module.exports = router;
